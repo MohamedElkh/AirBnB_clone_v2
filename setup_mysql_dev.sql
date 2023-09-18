496dc4faedb8071694deec80abaca3b806833f3d
mohamed <mohamedkharashy27@yahoo.com>
2023-09-18 12:51:28 -0700

    1

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
---setup
+--setup the script
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
+-- grant the pre
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
 FLUSH PRIVILEGES;
