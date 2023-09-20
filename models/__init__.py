52b7cc3345d785071807f3730425a5af31597a03
mohamed <mohamedkharashy27@yahoo.com>
2023-09-19 23:10:33 -0700

    1

--- models/__init__.py
+++ models/__init__.py
@@ -10,9 +10,9 @@ if var_st == 'db':
     from models.engine.db_storage import DBStorage
 
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
 
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
