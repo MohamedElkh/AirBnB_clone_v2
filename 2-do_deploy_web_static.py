86c1d0e20d8a19daf288f6234dbbe09149f823cc
mohamed <mohamedkharashy27@yahoo.com>
2023-10-04 14:24:15 -0700

    1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env, local, put, run, runs_once
 from datetime import datetime
 from os.path import exists
 
-
+""" the server of min """
 env.hosts = ['100.25.166.57', '54.173.193.255']
 env.user = 'ubuntu'
 env.key_namefile = '~/.ssh/school'
@@ -19,15 +19,15 @@ def do_deploy(archive_path):
         file_nx = archive_path.split("/")[-1]
         no_extx = file_nx.split(".")[0]
         path = "/data/web_static/releases/"
-
+        """archive to do """
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_extx))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_nx, path, no_extx))
         run('rm /tmp/{}'.format(file_nx))
-
+        """the same thing """
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extx))
         run('rm -rf {}{}/web_static'.format(path, no_extx))
-
+        """again then again"""
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_extx))
         return True
