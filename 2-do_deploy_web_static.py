83cc118a82540053f12face0ca4cf426feef6b56
mohamed <mohamedkharashy27@yahoo.com>
2023-10-04 13:53:46 -0700

    1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,27 +9,6 @@ env.hosts = ['100.25.166.57', '54.173.193.255']
 env.user = 'ubuntu'
 env.key_namefile = '~/.ssh/school'
 
-@runs_once
-def do_pack():
-    """create a func to achive the dict"""
-    dt = datetime.utcnow()
-
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
-
-
 def do_deploy(archive_path):
     """func to distribute archive web"""
     if not os.path.exists(archive_path):
